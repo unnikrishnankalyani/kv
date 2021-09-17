118cc94db42183387b1a04f12668758ca22e3df2
Kalyani Unnikrishnan <kalyani.iitkgp@gmail.com>
2021-09-17 00:27:50 -0500

    hash function

--- kv.c
+++ kv.c
@@ -1,9 +1,30 @@
 // kv
+
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <sys/types.h>
+//#include <unistd.h>
 
+#define HTABLESIZE 1000
+
+typedef struct{
+  int key;
+  char *value;
+}ht;
+
+ht* hash_table[HTABLESIZE];
+
+unsigned int hash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    //printf("%d: x before\n", x);
+    x = x % HTABLESIZE;
+    //printf("%d: x after\n", x);
+    return x ;
+}
 
 int main(int argc, char *argv[]){
   
@@ -15,7 +36,7 @@ int main(int argc, char *argv[]){
     ssize_t read;
     while ((read = getline(&line, &len, fptr)) != -1) {
         printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
+        printf("%s\n", line);
     }
     if(line)
       free(line);
@@ -36,6 +57,8 @@ int main(int argc, char *argv[]){
     char *v = strsep(&tempstr, ",");
 
     assert(tempstr == NULL);
+    int x = atoi(k);
+    printf("hash: %d\n ",hash(x));
     printf("%s   %s\n", k, v);
     fprintf(fptr,"%s,%s\n", k,v);
     free(tempstr);
