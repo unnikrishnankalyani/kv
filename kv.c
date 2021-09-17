7649de77652c9f59791bd9f61e1ac39c0fcd5f1e
Kalyani Unnikrishnan <kalyani.iitkgp@gmail.com>
2021-09-17 14:19:23 -0500

    get, delete, print, clear

--- kv.c
+++ kv.c
@@ -35,7 +35,7 @@ unsigned int hash(unsigned int x) {
 }
 
 hash_node *create_hash_node(int key, char *value){
-  printf("----trying to insert %d %s------\n", key, value);
+  //printf("----trying to insert %d %s------\n", key, value);
   hash_node *new_hn = (hash_node *) malloc(sizeof(hash_node));
   new_hn->key = key;
   value[strcspn(value, "\n")] = 0; //manage \n to avoid weird errors
@@ -43,6 +43,33 @@ hash_node *create_hash_node(int key, char *value){
   strcpy(new_hn->value, value);
   new_hn->next = NULL; //set this later if needed
   return new_hn;
+}
+
+void get_or_del(char *string, int b){
+  //b = 1 for delete
+  char *tempstr = strdup(string);
+  char *k  = strsep(&tempstr, ",");
+  assert(tempstr == NULL);
+  free(tempstr);
+  int x = atoi(k);
+  int hash_key = hash(x);
+
+  hash_node *temp_hn = ht[hash_key];
+
+  while (temp_hn!=NULL){
+    if (x == temp_hn->key){
+      if(b){
+        free(temp_hn);
+        temp_hn = NULL;
+      }else{
+        printf("%d,%s\n", temp_hn->key, temp_hn->value);
+      }
+        return;
+    }
+    temp_hn = temp_hn->next;
+  }
+  printf("%d not found\n", x);
+  return;
 }
 
 void put(char *string){
@@ -56,11 +83,11 @@ void put(char *string){
   int hash_key = hash(x);
   
   hash_node *h = ht[hash_key];
-  printf("hash_key = %d\n", hash_key);
+  //printf("hash_key = %d\n", hash_key);
   //if no entry, store and return
   if ( h == NULL){
     ht[hash_key] = create_hash_node(x,v);
-    printf("created new key: %d, %s\n", x, v);
+    //printf("created new key: %d, %s\n", x, v);
     return;
   } 
   
@@ -72,7 +99,7 @@ void put(char *string){
         free(temp_hn->value);
         temp_hn->value = (char *)malloc(sizeof(v));
         strcpy(temp_hn->value,v);
-        printf("replaced key: %d, %s\n", x, v);
+        //printf("replaced key: %d, %s\n", x, v);
         return;
     }
     prev = temp_hn;
@@ -80,7 +107,7 @@ void put(char *string){
   }
   //key does not exist, append to end of list (temp_hn is NULL, use prev)
   prev->next = create_hash_node(x,v);
-  printf("created new key: %d, %s\n", x, v);
+  //printf("created new key: %d, %s\n", x, v);
   
   return;
 }
@@ -94,8 +121,6 @@ void read_from_database(){
     size_t len = 0;
     ssize_t read;
     while ((read = getline(&line, &len, fptr)) != -1) {
-        printf("Retrieved line of length %zu:\n", read);
-        printf("%s\n", line);
         put(line);
     }
     if(line)
@@ -112,7 +137,7 @@ void print_table(){
     if(ht[i] != NULL){
       hash_node *temp_hn = ht[i];
       while (temp_hn!=NULL){
-        printf("%d: (%d, %s)\n", i, temp_hn->key, temp_hn->value);
+        printf("%d,%s\n", temp_hn->key, temp_hn->value);
         temp_hn = temp_hn->next;
       }
     }
@@ -149,20 +174,32 @@ int main(int argc, char *argv[]){
 
   //read in input kv pairs
   for (i=1; i<argc; i++){
-    printf("\n\n\n%s\n", argv[i]);
+    
     char *string = strdup(argv[i]);
     char *command  = strsep(&string, ",");
     
     switch(command[0]){
       case 'p':
-        put(string);   
+        put(string);
+        break;  
+      case 'g':
+        get_or_del(string, 0);
+        break;
+      case 'd':
+        get_or_del(string, 1);
+        break;
+      case 'a':
+        print_table();
+        break;
+      case 'c':
+        init_ht();
+        dump();
+        break;
       default:
         printf("bad command\n");
+        break;
     }
-
   }
-  print_table();
-
   //dump into file
   dump();
   return 0;
